fcadc47b272cf6eeb678ea8fb18e4f0552717ccf
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-07-03 23:10:24 +0200

    parse sentiment file and tweets

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,29 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def parse_afin(afinfile):
+    afinnfile = open(afinfile)
+    scores = {} # initialize an empty dictionary
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    print scores.items() # Print every (term, score) pair in the dictionary
+
+def parse_tweets(tweetfile):
+    tweets=[]
+    tweets_file=open(tweetfile)
+    for line in tweets_file:
+        tweet=json.loads(line)
+        print tweet["text"]
 
 def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    #parse_afin(sys.argv[1])
+    parse_tweets(sys.argv[2])
+    #lines(tweet_file)
 
 if __name__ == '__main__':
     main()
