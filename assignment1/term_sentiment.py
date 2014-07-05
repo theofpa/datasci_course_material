040b29c3bb0e52f7ea06052cc2a5c6a7cb9fc81b
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-07-05 23:31:18 +0200

    Derive the sentiment of new terms

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,17 +1,36 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def parse_afin(afinfile):
+    afinnfile = open(afinfile)
+    scores = {}
+    for line in afinnfile:
+        term, score  = line.split("\t")
+        scores[term] = int(score)
+    return scores
+
+def parse_tweets(tweetfile):
+    tweets=[]
+    tweets_file=open(tweetfile)
+    for line in tweets_file:
+        tweets.append(json.loads(line))
+    return tweets
 
 def lines(fp):
     print str(len(fp.readlines()))
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    scores=parse_afin(sys.argv[1])
+    tweets=parse_tweets(sys.argv[2])
+    for tweet in tweets:
+        total=0
+        for word in tweet["text"].split():
+            if word.encode('UTF8') in scores.keys():
+                total=total+scores[word]
+        #print total
+        for word in tweet["text"].split():
+            if word.encode('UTF8') not in scores.keys():
+                print word,", ", total
 
 if __name__ == '__main__':
     main()
