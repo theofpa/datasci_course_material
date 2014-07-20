0328b9b981e1287ebfb63bc6a8669e50b2e08fdb
Theofilos Papapanagiotou <theofilos@ieee.org>
2014-07-20 12:36:40 +0200

    social network analysis: friend count in mapreduce

---
+++ assignment3/friend_count.py
@@ -0,0 +1,33 @@
+import MapReduce
+import sys
+
+"""
+Friend Count Example in the Simple Python MapReduce Framework
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+def mapper(record):
+    # key: document identifier
+    # value: document contents
+    personA = record[0]
+    #personB = record[1]
+    mr.emit_intermediate(personA, 1)
+    #mr.emit_intermediate(personB, 1)
+
+def reducer(person, list_of_values):
+    # key: word
+    # value: list of occurrence counts
+    total = 0
+    for v in list_of_values:
+      total += v
+    mr.emit((person, total))
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
